5abf2414230a2c9a917fd5b54c6c0bcd629e94a3
James Andariese <james.andariese@locationlabs.com>
2013-06-04 21:27:28 -0700

    Added support for strings.

--- version_sort.go
+++ version_sort.go
@@ -1,11 +1,13 @@
 package version_sort
 
 import (
-//	"sort"
-	"strings"
-//	"math"
+	"bytes"
+	"fmt"
+	"regexp"
 	"strconv"
 )
+
+var _ = fmt.Println
 
 type ByVersion []string
 
@@ -17,43 +19,106 @@ func (va ByVersion) Swap(i, j int) {
 	va[i], va[j] = va[j], va[i]
 }
 
-func intize(str string, ret chan int32) {
-	defer close(ret)
-	numstrs := strings.Split(str, ".")
-	for _, v := range(numstrs) {
-		i, err := strconv.ParseUint(v, 10, 32)
-		if err != nil {
-			return
+// tokenize the string returning ints on one channel and strs on the other.
+// these are meant to be compared to another parallel tokenizer
+// if one channel closes first, it is less than the other unless the
+// currently waiting token is a string.
+// strings < nothing < int
+
+var versionRegexp = regexp.MustCompile("[0-9]+|\\.|[^0-9.]+")
+
+func tokenize(str string, retint chan int64, retstr chan string) {
+	defer close(retint)
+	defer close(retstr)
+	for _, v := range versionRegexp.FindAllString(str, -1) {
+		switch v[0] {
+		case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+			i, _ := strconv.ParseUint(v, 10, 64)
+			retint <- int64(i)
+		case '.':
+			// pass
+		default:
+			retstr <- v
 		}
-		ret<- int32(i)
 	}
 }
 
-func (va ByVersion) Less(i, j int) bool {
-	ic := make(chan int32)
-	jc := make(chan int32)
-	go intize(va[i], ic)
-	go intize(va[j], jc)
+func (va ByVersion) Less(i, j int) (ret bool) {
+	iic := make(chan int64)
+	isc := make(chan string)
+	jic := make(chan int64)
+	jsc := make(chan string)
+	go tokenize(va[i], iic, isc)
+	go tokenize(va[j], jic, jsc)
+
+	// if i is a string and j is a string, compare them
+	// if i is a string and j is an int, return true
+	// if i is a string and j is empty, return true
+	// if i is an int and j is a string, return false
+	// if i is an int and j is an int, compare them
+	// if i is an int and j is empty, return false
+	// if i is empty and j is a string, return false
+	// if i is empty and j is an int, return true
+	// if i is empty and j is empty, return false
 
 	for {
-		iv, iok := <-ic
-		jv, jok := <-jc
-		if !jok {
-			// !jok comes first because if they
-			// both have failed or if jok fails first,
-			// iok is not less than jok
-			// (if they fail at the same time, they are
-			// equal)
-			return false
-		}
-		if !iok {
-			return true
-		}
-		if iv < jv {
-			return true
-		}
-		if jv < iv {
-			return false
+		select {
+		case jiv, jiok := <-jic:
+			select {
+			case iiv, iiok := <-iic:
+				if !iiok {
+					if !jiok {
+						return false
+					}
+					return true
+				} else if !jiok {
+					return false
+				}
+				if iiv < jiv {
+					return true
+				}
+				if iiv > jiv {
+					return false
+				}
+			case _, isok := <-isc:
+				if !isok {
+					if !jiok {
+						return false
+					}
+					return true
+				}
+				return true
+			}
+		case jsv, jsok := <-jsc:
+			select {
+			case _, iiok := <-iic:
+				if !iiok {
+					if !jsok {
+						return false
+					}
+					return false
+				} else if !jsok {
+					return false
+				}
+				return false
+			case isv, isok := <-isc:
+				if !isok {
+					if !jsok {
+						return false
+					}
+					return false
+				} else if !jsok {
+					return true
+				}
+
+				cmp := bytes.Compare([]byte(isv), []byte(jsv))
+				if cmp < 0 {
+					return true
+				}
+				if cmp > 0 {
+					return false
+				}
+			}
 		}
 	}
 	return false // arbitrary.  we can never reach this.
--- version_sort_test.go
+++ version_sort_test.go
@@ -1,28 +1,31 @@
 package version_sort
 
 import (
-	"testing"
 	"sort"
+	"testing"
 )
 
 func TestLess(t *testing.T) {
-	strs := []string{"0.0", "1.1", "1.2.3.2", "1.2.3.4", "2",}
-	if ByVersion(strs).Less(1,0) {
+	strs := []string{"0.0", "1.1rc2", "1.1", "1.2.3.2", "1.2.3.4", "2.dev1", "2", "2.2.rc1", "2.2"}
+	if ByVersion(strs).Less(1, 0) {
 		t.Errorf("%#v > %#v but Less said it's not.\n", strs[1], strs[0])
 	}
-	for i, iv := range(strs) {
-		if ByVersion(strs).Less(i,i) {
+	for i, iv := range strs {
+		if ByVersion(strs).Less(i, i) {
 			t.Errorf("%#v is not less than itself\n", iv)
 		}
-		for j, jv := range(strs) {
-			if i < j {
-				if ByVersion(strs).Less(j,i) {
-					t.Errorf("%#v < %#v but Less said it's not\n", iv, jv)
-				}
-			} else if i > j {
-				if ByVersion(strs).Less(i,j) {
-					t.Errorf("%#v > %#v but Less said it's not\n", iv, jv)
-				}
+		for j, jv := range strs {
+			if i == j {
+				continue
+			}
+			if i < j && ByVersion(strs).Less(j, i) {
+				t.Errorf("%#v < %#v but Less said it's not\n", iv, jv)
+			}
+			if i > j && ByVersion(strs).Less(i, j) {
+				t.Errorf("%#v > %#v but Less said it's not\n", iv, jv)
+			}
+			if ByVersion(strs).Less(i, j) == ByVersion(strs).Less(j, i) {
+				t.Errorf("%#v <> %#v but Less said it's equal\n", iv, jv)
 			}
 		}
 	}
@@ -34,7 +37,7 @@ func TestSort(t *testing.T) {
 		"1.1",
 		"1.2.3.4",
 		"2",
-		"0.0",
+		"1.1dev",
 	}
 	sorted_strs := []string{
 		strs[4],
@@ -52,13 +55,13 @@ func TestSort(t *testing.T) {
 	}
 
 	sort.Sort(ByVersion(strs))
-	for k := range(strs) {
+	for k := range strs {
 		if strs[k] != sorted_strs[k] {
 			t.Errorf("%#v != %#v\n", strs, sorted_strs)
 			break
 		}
 	}
-	for k := range(strs) {
+	for k := range strs {
 		if strs[k] == fail_strs[k] {
 			t.Errorf("%#v != %#v\n", strs, fail_strs)
 			break
